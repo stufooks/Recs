f14631e784fe883880282946f79ff3ef91627a46
Stuart Fooks <sb.fooks@gmail.com>
2019-03-11 18:06:32 -0400

    simple sogn list screen

--- screens/MusicScreen.js
+++ screens/MusicScreen.js
@@ -1,15 +1,46 @@
 import React from 'react';
 import { ScrollView, StyleSheet, Text } from 'react-native';
 
+let songs = [
+  {
+    recID: 1,
+    track: "Brick",
+    artist: "(Sandy) Alex G"
+  },
+  {
+    recID: 2,
+    track: "Taste",
+    artist: "Forth Wanderers"
+  },
+  {
+    recID: 3,
+    track: "Pristine",
+    artist: "Snail Mail"
+  }
+]
+
 export default class LinksScreen extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      songs: songs
+    }
+
+  }
+
   static navigationOptions = {
     title: 'Links',
   };
 
   render() {
+
+    let songs = this.state.songs.map(song => {
+      return <Text key={song.recID}> {song.track} </Text>
+    })
     return (
       <ScrollView style={styles.container}>
-        <Text>Hello from Music</Text>
+        {songs}
       </ScrollView>
     );
   }
